e267e56be75283e3ac3785dfa9cb83ecefc6f8ea
Kerlyn <kerlynmanuel1@gmail.com>
2022-06-15 10:18:51 -0400

    changes

--- src/Components/Paisaje.js
+++ src/Components/Paisaje.js
@@ -3,8 +3,15 @@ import React from "react";
 function Paisaje()
 {
     return(
-        <div className='testimonio-container'>
-            <img className='testimonio-image' src={require('../images/Paisaje-Venecia.jpg')} alt='Imagen de Venecia'/>
+        <div className='country-description-container'>
+            <img className='country-image' src={require('../Images/Paisaje-Venecia.jpg')} alt='Imagen de Venecia'/>
+            <div className="country-text-container">
+                <p className="name-country">Venecia</p>
+                <p className="slogan-country">"La ciudad de los canales"</p>
+                <p className="description-country">"Venecia es la ciudad de los canales, capital de la región de Véneto, situada sobre un conjunto de islas que se extiende en una laguna pantanosa en el mar Adriático, entre las desembocaduras de los ríos Po al sur y Piave al norte."</p>
+            </div>
         </div>
     );
 }
+
+export default Paisaje;
