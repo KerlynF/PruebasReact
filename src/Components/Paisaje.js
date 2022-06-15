a593de14272acdf0d4c541567f927448e6d26b2a
Kerlyn <kerlynmanuel1@gmail.com>
2022-06-15 09:49:43 -0400

    landscape test

---
+++ src/Components/Paisaje.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Paisaje()
+{
+    return(
+        <div className='testimonio-container'>
+            <img className='testimonio-image' src={require('../images/Paisaje-Venecia.jpg')} alt='Imagen de Venecia'/>
+        </div>
+    );
+}
